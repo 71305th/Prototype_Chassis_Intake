66c760e5c9d7ded21925f70587ddd2e21aab0308
Hong <hongnai4507@gmail.com>
2023-02-03 23:31:43 +0800

    Intake refactor but haven't do button binding

---
+++ src/main/java/frc/robot/commands/Intake/IntakeEnums.java
@@ -0,0 +1,16 @@
+package frc.robot.commands.Intake;
+
+public class IntakeEnums {
+    public enum IntakeAction {
+        UP,
+        DOWN,
+        OPEN,
+        CLOSE,
+        RUN,
+        STOP,
+    }
+    public enum IntakeSide {
+        FRONT,
+        REAR,
+    }
+}
